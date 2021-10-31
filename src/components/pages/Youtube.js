1532ab3e3374b905d847ad8b69118c82b3edcd99
SangRo Lee <fashionfish@naver.com>
2021-10-31 14:02:15 +0900

    feat. Youtube page

--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -1,55 +1,77 @@
 import React, { useEffect, useState } from 'react';
+import gsap from 'gsap';
 import Header from '../include/Header';
 import Contents from '../include/Contents';
 import Footer from '../include/Footer';
 import WrapTitle from '../include/Wraptitle';
+import Loader from '../include/Loader';
 import YoutubeList from '../info/YoutubeList';
+import YoutubeSearch from '../info/YoutubeSerach';
 
 function Youtube() {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    var requestOptions = {
+  const start = () => {
+    setTimeout(() => {
+      gsap.set('.wrap__title h2 strong', { opacity: 0, y: 100 });
+      gsap.set('.wrap__title h2 em', { opacity: 0, y: 100 });
+      gsap.set('#header', { opacity: 0, top: '-100%' });
+      gsap.set('#footer', { opacity: 0, bottom: '-100%' });
+      gsap.set('.youtube__cont', { opacity: 0 });
+
+      gsap.to('.wrap__title h2 strong', { duration: 0.4, delay: 1, y: 0, opacity: 1, ease: 'power3.out' });
+      gsap.to('.wrap__title h2 em', { duration: 0.4, delay: 1.4, y: 0, opacity: 1, ease: 'power3.out' });
+      gsap.to('#header', { duration: 0.4, delay: 1.8, top: 0, opacity: 1, ease: 'power3.out' });
+      gsap.to('#footer', { duration: 0.4, delay: 1.8, bottom: 0, opacity: 1, ease: 'power3.out' });
+      gsap.to('.youtube__cont', { duration: 0.4, delay: 2, opacity: 1, ease: 'power3.out' });
+    }, 2000);
+  };
+
+  const search = (query) => {
+    const requestOptions = {
       method: 'GET',
       redirect: 'follow',
     };
-
     fetch(
-      'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=webstoryboy&type=video&key=AIzaSyAQO4--_qVneHIQKraWOxjPtRnRjHQ4kJs',
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=28&q=${query}&type=video&key=AIzaSyA0wLMn28hCflpbDVWPKiLU8MvtEV4_zXo`,
       requestOptions
     )
       .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+      .then((result) => {
+        setVideos(result.items);
+        start();
+      });
+  };
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=28&q=webstoryboy&type=video&key=AIzaSyA0wLMn28hCflpbDVWPKiLU8MvtEV4_zXo`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setVideos(result.items);
+        start();
+      });
   }, []);
 
   return (
     <div>
+      <Loader />
       <Header />
       <Contents>
         <section id="youtubeCont">
           <div className="container">
-            <WrapTitle text={['yotube', 'reference']} />
+            <WrapTitle text={['youtube', 'reference']} />
             <div className="youtube__cont">
+              <YoutubeSearch onSearch={search} />
               <div className="youtube">
-                <YoutubeList vidoes={videos} />
+                <YoutubeList videos={videos} />
               </div>
-            </div>
-          </div>
-        </section>
-
-        <section id="contactInfo">
-          <div className="contact__info">
-            <div>
-              <p>Let’s create something new</p>
-              <h3>
-                LET`S Get IN
-                <br />
-                TOUCH
-              </h3>
-              <a href="contact.html" className="contact">
-                Contact Me
-              </a>
             </div>
           </div>
         </section>
